763cef4f95b9e93aa3421837351c0489de66033d
luannt2809 <thanhluan2818@gmail.com>
2023-05-10 22:49:58 +0700

    connect_db

---
+++ models/database.js
@@ -0,0 +1,5 @@
+var mongoose = require('mongoose')
+
+mongoose.connect("mongodb+srv://luanntph26376:luan2809@cluster0.tr5kh33.mongodb.net/ql_thuvien").catch(err => console.error(err))
+
+module.exports = mongoose
