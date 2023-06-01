3eddc5903629b78af8336263cd504953d84d6eec
luannt2809 <thanhluan2818@gmail.com>
2023-06-01 15:30:49 +0700

    them_thong_ke

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,23 @@
 var express = require("express");
 var router = express.Router();
 var checkLogin = require("../middleware/check_login");
+var accountModel = require("../models/accounts.model");
+var billModel = require("../models/bill.model");
+var bookModel = require("../models/books.model");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+router.get("/", async function (req, res, next) {
+  let countAccount = await accountModel.accountModel.countDocuments({roleId: '645baaef738c215da807bae6'});
+  let countBook = await bookModel.bookModel.countDocuments();
+  let countBill = await billModel.ModelBill.countDocuments();
+  let bills = await billModel.ModelBill.find();
+
+  res.render("index", {
+    countAccount: countAccount,
+    countBook: countBook,
+    countBill: countBill,
+    bills: bills,
+  });
 });
 
 module.exports = router;
